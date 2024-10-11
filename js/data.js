a3e85142b1aeab05a0002549e78d9fff090493ef
Vand0525 <vand0525@algonquinlive.com>
2024-10-11 16:53:05 -0400

    finsished

--- js/data.js
+++ js/data.js
@@ -1,1 +1,14 @@
-export let enemyList = [];
+export let enemyList = [
+    {
+        uuid: 'fe3424fd-c2b2-461c-8e2f-dc7ed0220525',
+        name: 'Mother Teresa',
+        reason: 'Did not come to my birthday party.',
+    },
+    {
+        uuid: 'b7a40fcb-3ac6-41c1-8cda-06379f1dee6a',
+        name: 'Ghengis Khan',
+        reason: 'Called my brother mean things.',
+    },
+];
+
+// crypto.randomUUID();
--- js/main.js
+++ js/main.js
@@ -1,34 +1,86 @@
 import { enemyList } from './data.js';
+let enemies = [...enemyList]; // copy the contents of enemyList to a new array
 
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  //when the page loads...
-  buildEnemyCards();
-  addListeners();
+	//when the page loads...
+	buildEnemyCards();
+	addListeners();
 }
 
 function addListeners() {
-  //add a submit listener to the form
-  let form = document.querySelector('#enemyForm');
-  //add delete listeners for cards
-  let section = document.querySelector('#enemies');
+	//add a submit listener to the form
+	let form = document.querySelector('#enemyForm');
+	form.addEventListener('submit', addUserToList);
+  
+  document.getElementById('btnCancel').addEventListener('click', ev => {
+    ev.preventDefault();
+    document.getElementById('enemyForm').reset();
+  })
+	//add delete listeners for cards
+	let section = document.querySelector('#enemies');
+  section.addEventListener('click', removeFromList);
 }
 
 function buildEnemyCards() {
-  if (!enemyList) return;
-  let section = document.querySelector('#enemies');
-  //called when page loads AND after any update to the enemyList
+	if (!enemyList) return;
+	let section = document.querySelector('#enemies');
+
+	//called when page loads AND after any update to the enemyList
+	section.innerHTML = enemies
+		.map((enemy) => {
+			return `<div class="enemy" data-ref=${enemy.uuid}>
+        <h3>${enemy.name}</h3>
+        <p>${enemy.reason}</p>
+        <button class="btnDelete">Forgive Them</button>
+      </div>`;
+		})
+		.join('');
 }
 
-function addUserToList() {
-  //save the form data in the list
-  //remember to generate a uuid for each enemy
-  //rebuild the list of enemies cards
+function addUserToList(ev) {
+	ev.preventDefault();
+
+	const formData = new FormData(ev.target);
+  const name = formData.get('enemy');
+  const reason = formData.get('reason');
+
+  document.getElementById('enemy').classList.remove('error');
+  document.getElementById('reason').classList.remove('error');
+
+
+  if (!name || !reason) {
+    if (!name) {
+      document.getElementById('enemy').classList.add('error');
+    } 
+    if (!reason) {
+      document.getElementById('reason').classList.add('error'); 
+    }
+    return;
+  }
+
+	enemies.unshift({
+		uuid: crypto.randomUUID(),
+    name: name,
+    reason: reason,
+	});
+
+  ev.target.reset();
+	buildEnemyCards();
 }
 
-function removeFromList() {
-  //find the uuid in the card whose button was clicked
-  //remove from the enemyList
-  //rebuild the list of enemies cards
+function removeFromList(ev) {
+	let target = ev.target;
+  if(target.localName === 'button' && target.classList.contains('btnDelete')){
+    const card = target.closest('.enemy');
+    if(card){
+      const uuid = card.getAttribute('data-ref');
+      enemies = enemies.filter(enemy => {
+        if (uuid === enemy.uuid) return false;
+        return true;
+      });
+      buildEnemyCards();
+    } 
+  }
 }
